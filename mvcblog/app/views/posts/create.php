8d5898a6729ae4b687edc705597dd86476c2dfa5
Mohamed-Adel-work <84355991+Mohamed-Adel-work@users.noreply.github.com>
2022-02-08 11:09:02 +0200

    Add files via upload

---
+++ mvcblog/app/views/posts/create.php
@@ -0,0 +1,34 @@
+<?php
+require APPROOT . '/views/includes/head.php';
+?>
+<div class="navbar dark">
+    <?php
+    require APPROOT . '/views/includes/navigation.php';
+    ?>
+</div>
+
+<div class="container center">
+    <h1>
+        Create a new post
+    </h1>
+
+    <form action="<?php echo URLROOT; ?>/posts/create" method="POST">
+
+        <div class="form-item">
+            <input name="title" placeholder="Title..."></input>
+            <span class="invalidFeedback">
+                <?php echo $data['titleError'] ?>
+            </span>
+        </div>
+
+        <div class="form-item">
+            <textarea name="body" placeholder="Enter your post..."></textarea>
+            <span class="invalidFeedback">
+                <?php echo $data['bodyError'] ?>
+            </span>
+        </div>
+
+        <button class="btn green" name="submit" type="submit">Submit</button>
+    </form>
+
+</div>
